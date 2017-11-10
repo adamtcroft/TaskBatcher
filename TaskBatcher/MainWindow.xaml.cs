4ea4d6e655ea0d861c4e126a3c031882fbc6caab
Adam T. Croft <sendtoatc@gmail.com>
2017-11-09 16:48:10 -0800

    Get dates into listbox

--- TaskBatcher/MainWindow.xaml.cs
+++ TaskBatcher/MainWindow.xaml.cs
@@ -23,13 +23,58 @@ namespace TaskBatcher
         public MainWindow()
         {
             InitializeComponent();
-            GetCurrentMonday();
+            DateTime thisMonday = GetCurrentMonday();
+            DateTime nextMonday = thisMonday.AddDays(7);
+            DateTime thirdMonday = nextMonday.AddDays(7);
+
+            listBoxWeeks.Items.Insert(0, thisMonday.Date.ToString("d"));
+            listBoxWeeks.Items.Insert(1, nextMonday.Date.ToString("d"));
+            listBoxWeeks.Items.Insert(2, thirdMonday.Date.ToString("d"));
         }
 
-        public void GetCurrentMonday()
+        public DateTime GetCurrentMonday()
         {
             DateTime today = DateTime.Today;
-            MessageBox.Show(today.ToString());
+            DateTime thisMonday = new DateTime();
+
+            switch(today.DayOfWeek.ToString())
+            {
+                case "Monday":
+                    thisMonday = today;
+                    break;
+
+                case "Tuesday":
+                    thisMonday = today.AddDays(-1);
+                    break;
+
+                case "Wednesday":
+                    thisMonday = today.AddDays(-2);
+                    break;
+
+                case "Thursday":
+                    thisMonday = today.AddDays(-3);
+                    break;
+
+                case "Friday":
+                    thisMonday = today.AddDays(-4);
+                    break;
+
+                case "Saturday":
+                    thisMonday = today.AddDays(-5);
+                    break;
+
+                case "Sunday":
+                    thisMonday = today.AddDays(-6);
+                    break;
+            }
+            return thisMonday;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string newTask = taskToAdd.Text;
+
+
         }
     }
 }
