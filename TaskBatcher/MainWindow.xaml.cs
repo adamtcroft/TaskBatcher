9506de1d68e1a5b27c2bc6d9214666443513641b
Adam T. Croft <sendtoatc@gmail.com>
2017-11-10 09:14:09 -0800

    UI working with failsafes

--- TaskBatcher/MainWindow.xaml.cs
+++ TaskBatcher/MainWindow.xaml.cs
@@ -23,18 +23,54 @@ namespace TaskBatcher
         public MainWindow()
         {
             InitializeComponent();
-
+            List<DateTime> mondays = GetMondays();
 
 
             // Add first Monday to ListBox
-            listBoxWeeks.Items.Insert(0, thisMonday.Date.ToString("d"));
+            listBoxWeeks.Items.Insert(0, mondays[0].Date.ToString("d"));
             // Add second Monday to ListBox
-            listBoxWeeks.Items.Insert(1, nextMonday.Date.ToString("d"));
+            listBoxWeeks.Items.Insert(1, mondays[1].Date.ToString("d"));
             // Add third Monday to ListBox
-            listBoxWeeks.Items.Insert(2, thirdMonday.Date.ToString("d"));
+            listBoxWeeks.Items.Insert(2, mondays[2].Date.ToString("d"));
         }
 
-        public DateTime GetAllDays()
+  
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // Grab the task
+            string newTask = taskToAdd.Text;
+
+            // Get all of the checked days
+            List<string> selectedDays = GetDays();
+
+            // Get all of the selected weeks
+            List<string> selectedWeeks = GetWeeks();
+
+            // Check if any of our values are empty/null
+            if (isAnythingEmpty(newTask, selectedDays, selectedWeeks))
+                return;
+        }
+
+        private List<string> GetDays()
+        {
+            // Make a temporary list
+            List<string> selectedDays = new List<string>();
+
+            // For each checkbox item
+            foreach (CheckBox box in layoutGrid.Children.OfType<CheckBox>()) 
+            {
+                // IF the box is checked, add its value to our list
+                if(box.IsChecked == true)
+                {
+                    selectedDays.Add((string)box.Content);
+                }
+            }
+
+            // Hand off the list
+            return selectedDays;
+        }
+        private List<DateTime> GetMondays()
         {
             // Get the Monday of the current week
             DateTime thisMonday = GetCurrentMonday();
@@ -43,10 +79,15 @@ namespace TaskBatcher
             // Get the Monday of the 3rd week out
             DateTime thirdMonday = nextMonday.AddDays(7);
 
-            return thisMonday, nextMonday, thirdMonday;
-        }
+            List<DateTime> mondays = new List<DateTime>();
 
-        public DateTime GetCurrentMonday()
+            mondays.Add(thisMonday);
+            mondays.Add(nextMonday);
+            mondays.Add(thirdMonday);
+
+            return mondays;
+        }
+        private DateTime GetCurrentMonday()
         {
             // Get today's date
             DateTime today = DateTime.Today;
@@ -54,7 +95,7 @@ namespace TaskBatcher
             DateTime thisMonday = new DateTime();
 
             // If today's date is...
-            switch(today.DayOfWeek.ToString())
+            switch (today.DayOfWeek.ToString())
             {
                 // If it's Monday, save that!
                 case "Monday":
@@ -93,50 +134,43 @@ namespace TaskBatcher
             }
             return thisMonday;
         }
-
-        private void button_Click(object sender, RoutedEventArgs e)
+        private List<string> GetWeeks()
         {
-            // Grab the task
-            string newTask = taskToAdd.Text;
+            List<string> selectedWeeks = new List<string>();
+                
+            foreach(var item in listBoxWeeks.SelectedItems)
+            {
+                selectedWeeks.Add(item.ToString());
+            }
 
-            // Make a list for all checked days
-            List<string> selectedDays = new List<string>();
-
-            // Fill out our list of days
-            GetDays(selectedDays);
+            return selectedWeeks;
+        }
+        private Boolean isAnythingEmpty(string newTask, List<string> selectedDays, List<string> selectedWeeks)
+        {
+            Boolean empty = false;
 
             // If our task is empty, go back
             if (newTask == "")
             {
                 MessageBox.Show("You must enter a task to add!");
-                return;
+                empty = true;
             }
             
             // If no days were checked, go back
             if (!selectedDays.Any())
             {
                 MessageBox.Show("You must pick days to add your task to!");
-                return;
+                empty = true;
             }
 
-            foreach(Object item in listBoxWeeks.SelectedItems)
+            // If no weeks were selected, go back
+            if (!selectedWeeks.Any())
             {
-                
-            }
-        }
-
-        private List<string> GetDays(List<string> selectedDays)
-        {
-            foreach (CheckBox box in layoutGrid.Children.OfType<CheckBox>()) 
-            {
-                if(box.IsChecked == true)
-                {
-                    MessageBox.Show((string)box.Content);
-                    selectedDays.Add((string)box.Content);
-                }
+                MessageBox.Show("You must pick at least one week!");
+                empty = true;
             }
 
-            return selectedDays;
+            return empty;
         }
     }
 }
